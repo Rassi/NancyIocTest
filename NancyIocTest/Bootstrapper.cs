ef39bd19d92ab0f1b0860ccb68a716517c57f1d7
Rasmus Sigsgaard <rasmus@sigsgaard.com>
2015-08-26 21:16:56 +0200

    Initial commit

---
+++ NancyIocTest/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+
+namespace NancyIocTest
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            container.Register<IGreeter, Greeter>();
+            base.ConfigureRequestContainer(container, context);
+            //container.Register<IGreetingMessageService, GreetingMessageService>();
+        }
+    }
+}
---
+++ NancyIocTest/GreetingModule.cs
@@ -0,0 +1,56 @@
+﻿using Nancy;
+
+namespace NancyIocTest
+{
+    public interface IGreetingMessageService
+    {
+        string GetMessage();
+    }
+
+    public class GreetingMessageService : IGreetingMessageService
+    {
+        private static int _count = 0;
+        private readonly string _message;
+
+        public GreetingMessageService()
+        {
+            _message = "Hi from GreetingMessageService " + _count++;
+        }
+
+        public string GetMessage()
+        {
+            return _message;
+        }
+    }
+
+    public interface IGreeter
+    {
+        string Greet();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private readonly IGreetingMessageService _service;
+        private readonly string _message;
+        private static int _count = 0;
+
+        public Greeter(IGreetingMessageService service)
+        {
+            _service = service;
+            _message = "Hi from Greeter " + _count++;
+        }
+
+        public string Greet()
+        {
+            return _message + "<br>" + _service.GetMessage();
+        }
+    }
+
+    public class GreetingsModule : NancyModule
+    {
+        public GreetingsModule(IGreeter greeter)
+        {
+            Get["/"] = x => greeter.Greet();
+        }
+    }
+}
